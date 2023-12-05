dbe940cadba2b2fe0b6e65af76a9c7febf3703fe
chehr <chehrazad.ouazzani@student.vinci.be>
2023-12-05 07:48:09 +0100

    Rust learnings - enums

---
+++ exercises/intro/intro2.rs
@@ -0,0 +1,14 @@
+// intro2.rs
+//
+// Make the code print a greeting to the world.
+//
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
+// hint.
+
+
+fn main() {
+    println!("I AM NOT DONE");
+
+    let name = "Shera";
+    println!("Hello {name}!");
+}
